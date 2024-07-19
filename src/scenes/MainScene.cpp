f0dea9a9167500fcbac089770e05a9304b7859f1
Indatou <indatou@icible.ca>
2024-07-19 14:11:52 -0400

    Moving mouse with arrow keys

--- src/scenes/MainScene.cpp
+++ src/scenes/MainScene.cpp
@@ -12,4 +12,20 @@ void MainScene::update() {
     if (Keyboard::is_key_pressed(Key::TAB)) {
         Game::toggle_fullscreen();
     }
+    const auto speed = 200 * Time::delta();
+    Vector2 direction;
+    if (Keyboard::is_key_down(Key::UP)) {
+        direction += Vector2::UP;
+    }
+    if (Keyboard::is_key_down(Key::DOWN)) {
+        direction += Vector2::DOWN;
+    }
+    if (Keyboard::is_key_down(Key::LEFT)) {
+        direction += Vector2::LEFT;
+    }
+    if (Keyboard::is_key_down(Key::RIGHT)) {
+        direction += Vector2::RIGHT;
+    }
+    const auto new_position = Mouse::get_position() + direction * speed;
+    Mouse::set_position(new_position);
 }
