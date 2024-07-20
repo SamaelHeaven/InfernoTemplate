399450e98405793d8d0686bfba722ba3f023251a
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-07-20 01:22:29 -0400

    Added program arguments

--- src/game.cpp
+++ src/game.cpp
@@ -1,6 +1,6 @@
 #include "./scenes/MainScene.h"
 
-void main() {
+void main([[maybe_unused]] std::vector<std::string> args) {
     GameConfig config;
     config.icon = "textures/icon.png";
     file::set_working_directory("resources");
