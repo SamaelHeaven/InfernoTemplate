6a0fbb3f84fb39275169dedc0fdb5c07ab821ae6
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-07-20 01:14:04 -0400

    Changing cursor

--- src/scenes/MainScene.cpp
+++ src/scenes/MainScene.cpp
@@ -4,14 +4,26 @@ MainScene::MainScene() = default;
 
 MainScene::~MainScene() = default;
 
-void MainScene::initialize() {}
+void MainScene::initialize() {
+}
 
 void MainScene::update() {
+    // Clearing background
+
     Renderer::clear_background(Color::BLUE);
+
+    // Changing window title
+
     Game::set_title(std::string("FPS: ") + std::to_string(Time::current_fps()));
+
+    // Fullscreen toggle
+
     if (Keyboard::is_key_pressed(Key::TAB)) {
         Game::toggle_fullscreen();
     }
+
+    // Moving mouse
+
     const auto speed = 200 * Time::delta();
     Vector2 direction;
     if (Keyboard::is_key_down(Key::UP)) {
@@ -30,5 +42,22 @@ void MainScene::update() {
     const auto new_position = Mouse::get_position() + direction * speed;
     if (direction != Vector2::ZERO) {
         Mouse::set_position(new_position);
+    }
+
+    // Changing cursor
+
+    const auto cursor = Mouse::get_cursor();
+    for (int i = 0; i < static_cast<int32_t>(Key::NINE) + 1 - static_cast<int32_t>(Key::ZERO); i++) {
+        if (Keyboard::is_key_pressed(static_cast<Key>(static_cast<int32_t>(Key::ZERO) + i))) {
+            Mouse::set_cursor(static_cast<Cursor>(i + 1));
+        }
+    }
+
+    if (Keyboard::is_key_pressed(Key::BACKSPACE)) {
+        Mouse::set_cursor(Cursor::NONE);
+    }
+
+    if (Mouse::get_cursor() != cursor) {
+        std::cout << "Cursor: " << magic_enum::enum_name(Mouse::get_cursor()) << '\n';
     }
 }
