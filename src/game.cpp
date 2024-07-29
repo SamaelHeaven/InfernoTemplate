1deec50cc72059365273a4eee85fa454bf83d3a8
Indatou <indatou@icible.ca>
2024-07-29 14:44:12 -0400

    Refactor: Minor changes

--- src/game.cpp
+++ src/game.cpp
@@ -1,6 +1,6 @@
 #include "./scenes/MainScene.h"
 
-int32_t main([[maybe_unused]] std::vector<std::string> args) {
+int32_t main([[maybe_unused]] const std::vector<std::string> &args) {
     GameConfig config;
     config.icon = "textures/icon.png";
     file::set_working_directory("resources");
